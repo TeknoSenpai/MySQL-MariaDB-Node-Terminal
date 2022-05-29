023c2fe6b86eada91eb09e18a8f6d19e340de171
TeknoSenpai <74503731+TeknoSenpai@users.noreply.github.com>
2022-05-29 17:07:25 +0200

    Updated config.ts

--- src/interfaces/config.js
+++ src/interfaces/config.js
@@ -8,41 +8,17 @@
     GitHub: https://github.com/TeknoSenpai
     Discord: TeknoSenpai#0957
 */
-
-/*
-    Example config.json file:
-    {
-       "data": {
-           "silent_debug": false,
-           "no_color": false
-       },
-       "database_connection": {
-           "host": "localhost",
-           "user": "root",
-           "password": "",
-           "database": "mysql"
-    }
-}
-*/
-
 exports.__esModule = true;
-// @ts-ignore
 var config = require("../../config.json");
-var RED = require("../colors").RED;
+var colors_1 = require("../colors");
 switch (true) {
     case typeof config === undefined:
-        // @ts-ignore
-        console.log(RED + 'Please provide a valid config!') && process.exit(0);
-        break;
     case typeof config.database_connection === undefined:
-        // @ts-ignore
-        console.log(RED + 'Please provide a valid database config!') && process.exit(0);
-        break;
     case typeof config.database_connection.host === undefined || null:
     case typeof config.database_connection.user === undefined || null:
     case typeof config.database_connection.password === undefined || null:
     case typeof config.database_connection.database === undefined || null:
-        // @ts-ignore
-        console.log(RED + 'Please provide a valid database config!') && process.exit(0);
+        console.log(colors_1.RED + 'Please provide a valid database config!');
+        process.exit(0);
         break;
 }
